cccfc7f2cfdf9ca92df910ba642340a7804ff792
mykytam <morar.nikita@gmail.com>
2020-03-20 08:06:28 +0200

    Basic Dependency Injection added

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -6,9 +6,11 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"); // access applicationContext. xml file, read it and put Beans to Application Context
 
-        Music music = context.getBean("musicBean", Music.class); // Bean id name & class, Bean of which we want to get
+        //Music music = context.getBean("musicBean", Music.class); // Bean id name & class, Bean of which we want to get
+        //MusicPlayer musicPlayer = new MusicPlayer(music); // manual dependency injection
 
-        MusicPlayer musicPlayer = new MusicPlayer(music); // manual dependency injection
+        // Spring dependency injection
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); // getting musicPlayer from applicationContext & class, Bean of which we want to get
         musicPlayer.playMusic();
 
         context.close(); // when finished working with Application Context, connection must be closed
