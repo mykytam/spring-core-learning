67aed04637ccf34cdc2026fda483e80d7d7633df
mykytam <morar.nikita@gmail.com>
2020-03-21 15:46:53 +0200

    Bean scope

--- src/main/java/TestSpring.java
+++ src/main/java/TestSpring.java
@@ -10,15 +10,19 @@ public class TestSpring {
         //MusicPlayer musicPlayer = new MusicPlayer(music); // manual dependency injection
 
         // Spring dependency injection
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); // getting musicPlayer from applicationContext & class, Bean of which we want to get
-        musicPlayer.playMusic();
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class); // getting musicPlayer from applicationContext & class, Bean of which we want to get
+        MusicPlayer secondMusicPlayer  = context.getBean("musicPlayer", MusicPlayer.class);
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer; // pointer comparison
+        System.out.println(comparison); // true, hash is equal. singleton works
+        firstMusicPlayer.setVolume(10); // firstMusicPlayer's  and firstMusicPlayer's volume will be 10
 
         // What happens when adding DI with setter
         // MusicPlayer musicPlayer1 = new musicPlayer();  -  creating bean with empty constructor
         // musicPlayer1.setMusic();  -  using setter and transmitting bean
 
-        System.out.println(musicPlayer.getName()); // DI with setter done
-        System.out.println(musicPlayer.getVolume()); // DI with setter done
+        //System.out.println(musicPlayer.getName()); // DI with setter done
+        //System.out.println(musicPlayer.getVolume()); // DI with setter done
 
         context.close(); // when finished working with Application Context, connection must be closed
     }
