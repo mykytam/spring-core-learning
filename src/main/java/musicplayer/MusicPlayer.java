42a53292f96afe6a5e13b32342c3eb38e7af52f0
mykytam <morar.nikita@gmail.com>
2020-03-23 12:11:36 +0200

    Annotations @Value and @Scopee

--- src/main/java/musicplayer/MusicPlayer.java
+++ src/main/java/musicplayer/MusicPlayer.java
@@ -2,12 +2,34 @@ package musicplayer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
     private Music music1;
     private Music music2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     @Autowired
     public MusicPlayer (@Qualifier("rockMusic") Music music1,
