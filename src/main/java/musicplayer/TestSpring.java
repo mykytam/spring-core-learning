12d95c8d7e13d35eed78072719ae7a1ad3a9dadd
mykytam <morar.nikita@gmail.com>
2020-03-23 11:36:27 +0200

    Adding DI with annotation @Autowired

--- src/main/java/musicplayer/TestSpring.java
+++ src/main/java/musicplayer/TestSpring.java
@@ -8,16 +8,22 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"); // access applicationContext. xml file, read it and put Beans to Application Context
 
-        Music music1 = context.getBean("rockMusic", Music.class); // Bean id (name of class) & class, Bean of which we want to get
-        MusicPlayer rockPlayer = new MusicPlayer(music1); // manual dependency injection
-        rockPlayer.playMusic();
+//        Music music1 = context.getBean("rockMusic", Music.class); // Bean id (name of class) & class, Bean of which we want to get
+//        MusicPlayer rockPlayer = new MusicPlayer(music1); // manual dependency injection
+//        rockPlayer.playMusic();
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class); // Bean id (name of class) & class, Bean of which we want to get
+//        MusicPlayer classicalPlayer = new MusicPlayer(music2); // manual dependency injection
+//        classicalPlayer.playMusic();
 
-        Music music2 = context.getBean("classicalMusic", Music.class); // Bean id (name of class) & class, Bean of which we want to get
-        MusicPlayer classicalPlayer = new MusicPlayer(music2); // manual dependency injection
-        classicalPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class); // getting musicPlayer from context
+//        musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close(); // when finished working with Application Context, connection must be closed
-        
+
     }
 
 }
